c21df2e4ac7fb0d29819d4d893cba540f9aeda09
SarahKamunya <sarahkamunya254@gmail.com>
2022-05-11 14:55:37 +0300

    add comment class and pass db.model, add tablename variable for comments

--- app/models.py
+++ app/models.py
@@ -27,6 +27,46 @@ class Pitches(db.Model):
     up_vote = db.relationship('Upvote', backref='pitch', lazy='dynamic')
     down_vote = db.relationship('Downvote', backref='pitch', lazy='dynamic')
     
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+        
+    @classmethod
+    def get_category(cls, category):
+        pitches = Pitches.query.filter_by(category=category).all()
+        return pitches    
+        
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()    
+
+    def __repr__(self):
+        return f'User {self.title}'
+    
+class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
+    comment = db.Column(db.Text())
+    
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls, post_id):
+        comments = Comment.query.filter_by(post_id=post_id).all()
+        return comments
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'Comments: {self.comment}'
+        
+    
     
     
         
