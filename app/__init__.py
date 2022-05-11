203d8dac82b435a303671bc1ec9fde38089137a0
SarahKamunya <sarahkamunya254@gmail.com>
2022-05-11 07:34:01 +0300

    install flask-script,import create-app function, import manager clas from flask script

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
 # from config import config_options
 from flask_bootstrap import Bootstrap
+from config import config_options
 
+app = Flask(__name__)
 
 bootstrap = Bootstrap()
 
 def create_app(config_name):
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+    from .main import main as main_blueprint
     
-    app = Flask(__name__)
+  
     
     
     return app
--- manage.py
+++ manage.py
@@ -1,8 +1,8 @@
-# from app import create_app,db
-# from flask_script import Manager,Server
-# from app.models import User
-# from flask_migrate import Migrate, MigrateCommand
-# #Creating app instance
+from app import create_app,db
+from flask_script import Manager,Server
+from app.models import User
+from flask_migrate import Migrate, MigrateCommand
+#Creating app instance
 # app = create_app('production')
 # manager = Manager(app)
 # manager.add_command('server',Server)
