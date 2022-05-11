4925010a7270db71ce07eb3dcae2c0cf77d2ac8e
SarahKamunya <sarahkamunya254@gmail.com>
2022-05-11 20:24:42 +0300

    add sigup form,signup route and signup.html file

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,22 @@
-from flask import render_template
+from flask import render_template,url_for,redirect
 from . import auth
+from ..models import User
+from .forms import SignUpForm
+from .. import db
+from ..email import mail_message
 
 @auth.route('/login')
 def login():
     return render_template('auth/login.html')
+
+@auth.route('/signup', methods=['GET', 'POST'])
+def register():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        mail_message('Welcome to pitch', 'email/welcome_user', user.email, user=user)
+        return redirect(url_for('auth.login'))
+        title = "Create account"
+    return render_template('auth/signup.html', signup_form=form)
--- app/models.py
+++ app/models.py
@@ -1,9 +1,9 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin, current_user
-# from . import login_manager
+from . import login_manager
 
-# @login_manager.user_loader
+@login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
