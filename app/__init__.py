d619fdcd10b6458df70e33bdb5cb202568d84cf0
SarahKamunya <sarahkamunya254@gmail.com>
2022-05-11 13:48:25 +0300

    import db in models.py, add user class for new users and pass db.model argument, add tablename variable for users

--- app/__init__.py
+++ app/__init__.py
@@ -10,9 +10,11 @@ def create_app(config_name):
     app = Flask(__name__)
     # Creating the app configurations
     app.config.from_object(config_options[config_name])
+    
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    
     from .main import main as main_blueprint
-    
-  
-    
-    
+       
     return app
