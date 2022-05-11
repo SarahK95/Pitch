d07f503a3bf7ce92881f0a5c279abe60ca6fdb9e
SarahKamunya <sarahkamunya254@gmail.com>
2022-05-11 18:32:57 +0300

    add main route for index and new-pitch, add filter for different categories

--- app/main/views.py
+++ app/main/views.py
@@ -1,26 +1,42 @@
+from unicodedata import category
 from flask import render_template,redirect, url_for
 from . import main
 from .forms import PitchForm, CommentForm
-
+from flask_login import login_required, current_user
+from ..models import Pitches, Comment, User, Upvote, Downvote
 
 @main.route('/')
 def index():
-    '''
-    View root page function that returns the index page and its data
-    '''
-    return render_template('index.html')
+    pitches=Pitches.query.all()
+    product=Pitches.query.filter_by(category='product').all()
+    pickup=Pitches.query.filter_by(category='pickup').all()
+    interview=Pitches.query.filter_by(category='interview').all()
+    promotion=Pitches.query.filter_by(category='promotion').all()
+    
+    return render_template('index.html', pitches=pitches ,product=product, pickup=pickup, interview=interview, promotion=promotion)
+
+@main.route('/pitches')
+@login_required
+def pitches():
+    pitches= Pitches.query.all()
+    upvote = Upvote.query.all()
+    user = current_user
+    return render_template('pitch.html', pitches=pitches, upvote=upvote, user=user)
+
+@main.route('/new_pitch', methods=['GET', 'POST'])
+@login_required
+def new_pitch():
+    form = PitchForm()
+    if form.validate_on_submit():
+        title = form.title.data
+        pitch = form.pitch.data
+        category = form.category.data
+        user_id = current_user._get_current_object().id
+        new_pitch = Pitches(pitch=pitch, title=title, category=category, user_id=user_id)
+        new_pitch.save()
+        return redirect(url_for('main.index'))
+    return render_template('pitch.html', form=form)
 
 
-# @main.route('/new_pitch', methods=['GET','POST'])
-# def pitch_form():
-#     form = PitchForm()
-#     if form.validate_on_submit():
-#         title = form.title.data
-#         pitch = form.pitch.data
-#         category = form.category.data
-#         user_id = current_user._get_current_object().id
-#         new_pitch = Pitches(pitch=pitch,title=title, category=category)
-#         new_pitch.save_pitch()
-#         return redirect(url_for('main.index'))
-#     return render_template('pitch.html', form=form)
+
         
