17dd461f0ac763bf83950660cab0c3f72c450944
SarahKamunya <sarahkamunya254@gmail.com>
2022-05-11 14:29:30 +0300

    add pitches class and pass db.model ,add tablename variable for pitches

--- app/models.py
+++ app/models.py
@@ -15,3 +15,18 @@ class User(db.Model):
 
     def __repr__(self):
         return f'User {self.username}'
+    
+class Pitches(db.Model):
+    __tablename__ = 'pitches'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    pitch = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    category = db.Column(db.String, nullable=False)
+    comment = db.relationship('Comment', backref='pitch', lazy='dynamic')
+    up_vote = db.relationship('Upvote', backref='pitch', lazy='dynamic')
+    down_vote = db.relationship('Downvote', backref='pitch', lazy='dynamic')
+    
+    
+    
+        
