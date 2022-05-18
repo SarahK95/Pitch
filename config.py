d04cbe70d2500e6232ce7cc46e807353cbcf3997
SarahKamunya <sarahkamunya254@gmail.com>
2022-05-19 00:58:41 +0300

    export sqlalchemy uri to start.sh

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 class Config:
    
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
     
     
     #  email configurations
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export DATABASE_URL='postgresql+psycopg2://ben:ben10@localhost/pitch'
+export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://ben:ben10@localhost/pitch'
 export MAIL_USERNAME='sarahnje82@gmail.com'
 export MAIL_PASSWORD='comenakinini'
 export SECRET_KEY='secret'
